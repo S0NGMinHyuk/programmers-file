399c13a54ef13528d73de761ad3bb15d282e3c7c
S0NGMinHyuk <poiuy010724@naver.com>
2021-09-18 17:40:12 +0900

    2021.08.25
    문제를 거의 다 풀었다 생각했지만 의문의 테스트케이스 10번을 시간초과로 풀지 못한 문제였다.
    탐욕 알고리즘을 사용하는 문제라고 써있었지만 스택 알고리즘을 통해 푸는게 유명한 문제다.
    이 외에도 문자열의 인덱스만을 다루며 푸는 방법도 있다고 했는데 그 방법은 무엇인지 잘 모르겠다.
    
    나름 코드를 작성하기 전 방향을 잡고 시작했지만 결과는 생각보다 난잡했다. for문을 여러번 사용하면서
    시간 초과가 난 것 같지만 해결은 하지 못했다.
    
    스택을 사용한 풀이를 보니 확실히 코드가 짧고 이해하기도 쉽게 쓰여져있다고 느끼며
    스택 알고리즘을 모르는 입장이지만 어떤 방식으로 사용하는 지 어렴풋이 알 것 같기도 하다.
    
    언제쯤 이런 멋진 코드를 쓸 수 있을까.
    
    2021.09.18
    
    프로그래머스의 실력체크를 풀며 이 문제를 다시 만났었다. 8월 25일에도 결국 다른 사람의 코드를 보고 풀었던 거라 이번에 다시 풀어보게 됐을 때
    그때와 똑같이 헤메고 헤메다 결국 이전의 답을 보고 풀게 되었다.
    
    대대적으로 다른 사람의 코드를 보고 푼 문제를 다시 풀어봐야겠다. 저번과 똑같이 문제를 헤메다 못 풀게 되니 슬프다.

--- 큰수만들기.py
+++ 큰수만들기.py
@@ -2,66 +2,14 @@ def solution(number, k):
     # stack에 입력값을 순서대로 삽입 
     stack = [number[0]]
     for num in number[1:]:
-        # 들어오는 값이 stack 값보다 크면, 기존의 값을 제거하고 새로운 값으로 바꿈 
-        # 참고 : len(stack) > 0 == stack
         while len(stack) > 0 and stack[-1] < num and k > 0:
-            # 값을 한개 빼서 k는 1이 제거 
-            k -= 1
-            # 내부의 값을 제거 
-            stack.pop()
-        
-        # while 문 조건이 맞지 않을 경우 새로운 값을 삽입 
-        stack.append(num)
+        # 들어오는 값이 stack 값보다 크면, 기존의 값을 제거하고 새로운 값으로 바꿈 
+            k -= 1 # 값을 한개 빼서 k는 1이 제거 
+            stack.pop() # 내부의 값을 제거 
 
-    # 만일 충분히 제거하지 못했으면 남은 부분은 단순하게 삭제
-    # 이렇게 해도 되는 이유는 이미 큰 수부터 앞에서 채워넣었기 때문 
+        stack.append(num)
+        # 새로운 값을 삽입 
     if k != 0:
         stack = stack[:-k]
-    
+    # 만일 k 만큼 제거하지 못했으면 남은 부분은 뒤부터 삭제
     return ''.join(stack)
-
-#--------------------------------------------------------------------------------------
-
-def solution(number, k):
-    # 넘버 문자열을 리스트로 변환
-    number = list(number)
-
-    # 만일 한가지 숫자로만 이뤄진 경우 빠르게 반환 (테스트케이스 10번용 but 실패)
-    if len(set(number)) == 1:  
-        return number[0]*(len(number)-k)
-
-    # 최대값을 시작할 첫 번째 자릿수를 정하기 위한 코드
-    big = "0"
-    for i in range(k):
-        # 만일 9가 나오면 이하 내용 스킵
-        if number[i] == "9":
-            number = number[i:]
-            k -= i
-            break
-        elif number[i] > big:
-            big, index = number[i], i
-    else:
-        # for 문을 다 돌고 난 후 정리
-        number = number[index:]
-        k -= index
-    
-    # 남은 k 값만큼 작은 수를 없애기 위한 코드
-    start = 0
-    while k:
-        for i in range(start, len(number)):
-            try:
-                if number[i] < number[i+1]:
-                    number.pop(i)
-                    k -= 1
-                    # 이미 반복한 코드를 다시 반복하기 않기 위해 사용
-                    start = i -1
-                    break
-                else:
-                    None
-            except:
-                # k 가 남았을 경우 맨 뒷 자리부터 k 만큼 삭제
-                number = number[:-k]
-                k = 0
-                break
-    
-    return "".join(number)
