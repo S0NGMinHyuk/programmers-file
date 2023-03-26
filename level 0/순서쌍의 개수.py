f2932aa25a27f1c5e334f67256b5a4d473bf053f
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2023-03-26 22:26:45 +0900

    2023.03.26

---
+++ level 0/순서쌍의 개수.py
@@ -0,0 +1,10 @@
+def solution(n):
+    if n**0.5 == int(n**0.5):
+        answer = -1
+    else:
+        answer = 0
+        
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            answer += 2
+    return answer
