264dac3bd50ba012dfc3f9add2d3013ac80dba01
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2023-03-31 22:22:57 +0900

    2023.03.31
    기존 코드에 주석을 달며 코드 이해를 했다.
    34~36번 줄 코드를 생각했었지만 위치를 26번 줄에 작성하는 바람에 풀이에 실패했었다.

--- level 1/개인정보 수집 유효기간.py
+++ level 1/개인정보 수집 유효기간.py
@@ -1,6 +1,9 @@
 def solution(today, terms, privacies):
+    # today 데이터 정제
     td = today.split(".")
     td = [int(i) for i in td]
+
+    # 약관 유효기간 딕셔너리 생성
     term = {}
     for t in terms:
         a = t.split()
@@ -9,23 +12,30 @@ def solution(today, terms, privacies):
     result = []
     for o, p in enumerate(privacies):
         a = p.split()
-        keeping = term[a[1]] # 보관기간간
+        keeping = term[a[1]] # 보관기간
+
+        # date 변수 생성 및 정제
         date = a[0].split(".")
         date = [int(i) for i in date]
 
+        # date 의 날짜를 하루 전으로 변경
         if date[2] > 1:
             date[2] -= 1
         else:
-            date[2] = 28 ; date[1] -= 1
+            date[2] = 28 ; date[1] -= 1 
+            # keeping 이 1 이상이기 때문에 date[1]이 0이어도 상관없다.
         
+        # date 의 달을 keeping 만큼 증가
         date[1] += keeping
         if date[1] > 12:
             date[0] += date[1] // 12
             date[1] = date[1] % 12
+            # date[1]이 0일 경우 예외처리
             if date[1] == 0:
                 date[1] = 12
                 date[0] -= 1
         
+        # 오늘 날짜와 비교 후 result 리스트에 추가 or 스킵
         if td[0] > date[0]:
             result.append(o+1)
         elif td[0] == date[0] and td[1] > date[1]:
