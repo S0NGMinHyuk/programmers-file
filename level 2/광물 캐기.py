a1db9f1cb61aadd84d71547d7ed8640443c52543
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2023-04-05 15:38:30 +0900

    2023.04.05
    탐욕(그리디) 알고리즘을 사용해 푸는 문제였다.
    어제 문제 풀이 방향은 제대로 풀었는데 33번째 줄 picks를 가공하는 과정에서 오류가 있었다.
    코드를 한줄 한줄 읽어보며 직관적이지 않은 부분이나 오류가 일어날 만 한 코드를 변경해보면서 답을 찾을 수 있었다.
    당일에 문제 해결은 하지 못했지만 내가 계획했던 풀이 방향이 틀리지 않았다는 점은 만족스럽다.

--- level 2/광물 캐기.py
+++ level 2/광물 캐기.py
@@ -1,6 +1,7 @@
 def solution(picks, minerals):
-    dia = [] ; iron = [] ; stone = []
+    hp = []
     for slicing in range(sum(picks)):
+        # 광물을 5개씩 슬라이싱 후 돌곡괭이 기준 피로도로 변경
         m = minerals[slicing * 5:slicing * 5 + 5]
         for i in range(len(m)):
             if m[i] == "diamond":
@@ -9,39 +10,42 @@ def solution(picks, minerals):
                 m[i] = 5
             else:
                 m[i] = 1
+        
+        # 5개씩 나눠져 있는 피로도를 각 곡괭이로 캘 때의 피로도로 치환
+        # hp 리스트에 [stone, iron, diamond] 리스트 추가
         if m:
-            dia.append(len(m))
-            stone.append(sum(m))
-            temp = 0
+            stone = sum(m)
+            iron = 0
             for i in m:
                 if i == 1:
-                    temp += 1
+                    iron += 1
                 else:
-                    temp += i//5
-            iron.append(temp)
+                    iron += i//5
+            hp.append([stone, iron, len(m)])
+        # m이 빈 리스트인 경우는 주어진 곡괭이에 비해 캘 광물이 적은 경우
+        # 슬라이싱 반복문 탈출
         else:
             break
     
-    if len(dia) < sum(picks):
-        skip = sum(picks) - len(dia)
-        for i in range(2, 0, -1):
-            if picks[i] and skip:
-                if picks[i] >= skip:
-                    picks[i] -= skip
-                    skip = 0
-                else:
-                    skip -= picks[i]
-                    picks[i] = 0
-            else:
-                None
-    
+    # 돌곡괭이 피로도 기준으로 내림차순 정렬
+    hp.sort(key=lambda x : x[0], reverse=True)
 
-    dic = { 0:stone, 1:iron, 2:dia }
+    # 주어진 곡괭이(picks) 중 사용할 곡괭이 계산
+    can_use = len(hp)
+    for i in range(len(picks)):
+        if can_use:
+            if picks[i] <= can_use:
+                can_use -= picks[i]
+            else:
+                picks[i] = can_use
+                can_use = 0
+        else:
+            picks[i] = 0
+
+    # 낮은 품질의 곡괭이부터 사용
     answer = 0
     for i, num in enumerate(reversed(picks)):
         for _ in range(num):
-            temp = min(dic[i])
-            ind = dic[i].index(temp)
-            dia[ind] = iron[ind] = stone[ind] = 126
-            answer += temp
+            answer += hp.pop()[i]
+            
     return answer
