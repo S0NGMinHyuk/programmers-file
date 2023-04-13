94453d2e90a24ae759c881c4c2ac6d8ad098b737
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2023-04-13 19:33:26 +0900

    2023.04.13
    너비 우선 탐색 알고리즘에 대한 해답을 보고 풀었다.
    해석을 보지 않았더라면 더욱 고생했을 것 같다. 그러나 좋은 해답 덕분에 여러가지 방법을 배웠다.
    
    1. 방문 여부를 체크하기 위해 visited 2차원 리스트를 만든다는 점
    2. dx, dy 리스트를 통해 상하좌우를 체크할 수 있게 한 점
    3. 0 <= nx < r and 0 <= ny < c 조건식을 통해 인덱스 오류를 방지한 점
    4. queue에 대한 약간의 개념과 bfs 알고리즘에 대한 개념
    
    위의 4가지 요소가 특히 놀라웠고 이번 문제를 통해 2차원 리스트 (그래프) 문제 접근 방법을 조금은 알게 된 것 같다. 
    아직 그래프 탐색 문제는 많으니 다음번엔 꼭 풀어낼 것이다.

--- level 2/무인도 여행.py
+++ level 2/무인도 여행.py
@@ -1,49 +1,38 @@
-def solution(maps):
-    # 인덱스 오류 방지를 위한 maps 가공
-    temp = []
-    for i in maps:
-        i = "X" + i + "X"
-        temp.append(list(i))
-    margin = ["X"] * (len(maps[0]) + 2)
-    temp.insert(0, margin) ; temp.append(margin)
-    maps = temp
-    del(margin) ; del(temp)
+def bfs(i, j, r, c, visited, maps):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    deque = [[i,j]]
+    visited[i][j] = 1
+    cnt = int(maps[i][j])
 
-    island = []
-    for column in range(1, len(maps) - 1):
-        for row in range(1, len(maps[0]) - 1):
-            if maps[column][row] != "X":
-                island.append(island_check(maps, column, row))
+    while deque:
+        cx, cy = deque.pop(0)
+        for i in range(4):
+            # maps[cx][cy]의 상하좌우 체크
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+
+            # 범위 선언 디테일 체크
+            if 0 <= nx < r and 0 <= ny < c:
+                if visited[nx][ny] == 0 and maps[nx][ny] != "X":
+                    deque.append([nx, ny])
+                    visited[nx][ny] = 1
+                    cnt += int(maps[nx][ny])
     
-    return sorted(island) if island else [-1]
+    return visited, cnt
 
 
-
-def island_check(maps, x, y):
-    cnt = int(maps[x][y])
-    maps[x][y] = "X"
-
-    # 상하좌우 체크
-    if left(maps, x, y):
-        cnt += island_check(maps, x, y - 1)
-    if right(maps, x, y):
-        cnt += island_check(maps, x, y + 1)
-    if up(maps, x, y):
-        cnt += island_check(maps, x - 1, y)
-    if down(maps, x, y):
-        cnt += island_check(maps, x + 1, y)
-
-    return cnt
-
-
-def left(maps, x, y):
-    return True if maps[x][y - 1] != "X" else False
-
-def right(maps, x, y):
-    return True if maps[x][y + 1] != "X" else False
-
-def up(maps, x, y):
-    return True if maps[x - 1][y] != "X" else False
-
-def down(maps, x, y):
-    return True if maps[x + 1][y] != "X" else False
+def solution(maps):
+    r, c = len(maps), len(maps[0])
+    # 방문 확인용 2차원 리스트 생성
+    visited = [[0]*c for _ in range(r)]
+    answer = []
+    
+    for i in range(r):
+        for j in range(c):
+            if maps[i][j] != "X" and visited[i][j] == 0:
+                visited, cnt = bfs(i, j, r, c, visited, maps)
+                answer.append(cnt)
+    
+    # answer가 빈 값일 경우 예외처리
+    return sorted(answer) if answer else [-1]
