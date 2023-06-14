0561800cfe091cb66483389b29e5f458b15e35c1
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2023-06-14 22:06:40 +0900

    2023.06.14
    지난 번에 배웠던 누적합 풀이 의사코드를 보고 다시 한 번 도전했다. 
    2번째 코드가 내 풀이였는데 여전히 효율성을 통과하지 못했다. 
    
    넘파이 모듈까지 사용하며 내가 할 수 있는 최대한 빠른 코드를 만들었지만 그럼에도 효율성을 통과하지 못했기에 내 풀이 알고리즘이 애초에 잘못됐음을 느끼고 다른 사람의 풀이를 참고했다.
    
    내 풀이 방법의 가장 큰 문제는 각각의 skill 때마다 누적합 계산을 돌리고 board에 업데이트했기 때문에 당연히 시간초과가 날 수 밖에 없었다.
    매번 누적합 계산을 하는 게 아니라 각 skill의 누적합 초기값을 배열에 추가하고 한꺼번에 누적합 계산을 해 누적합 계산과 board에 업데이트를 1번만 해야 했었다.
    누적합 알고리즘에 대한 이해도가 낮아서 풀지 못했던 것 같다. 
    
    이번 문제를 풀면서 그동안 멀리했던 넘파이 모듈도 사용해봤고, 누적합에 대해 찾아보며 파이썬의 다른 기능들도 알게 되었다. 또한 코딩테스트 문제에 넘파이 모듈은 쓰는게 아닌 것 같다는 개인적인 의견도 생겼다. 문제가 어려웠고 오랜 시간 물고 늘어졌지만 재밌고 의미있는 시간이었다.

--- level 3/파괴되지 않은 건물.py
+++ level 3/파괴되지 않은 건물.py
@@ -15,3 +15,82 @@ def solution(board, skill):     # 정확성 통과, 효율성 실패
                         bldg += 1
                     board[row][col] += degree
     return bldg
+
+#_________________________________________________________________________________________________________________
+import numpy as np  # 누적합 알고리즘 사용, 그러나 계속 효율성 실패
+
+def make_degree(info, grid):    # skill별 누적합 2차원배열 생성 함수
+    arr = grid.copy()
+    # 누적합 초기값 설정
+    arr[info[1]][info[2]], arr[info[3]+1][info[4]+1] = info[5], info[5]
+    arr[info[1]][info[4]+1], arr[info[3]+1][info[2]] = -info[5], -info[5]
+
+    # 행열 누적합 계산
+    arr = arr.cumsum(axis=0)
+    arr = arr.cumsum(axis=1)
+
+    # 누적합 계산을 위해 만들어뒀던 잉여 행열 제거
+    arr = np.delete(arr, -1, axis=0)
+    arr = np.delete(arr, -1, axis=1)
+
+    return arr  # 2차원 배열 리턴
+
+
+def solution(board, skill):
+    # 넘파이 배열로 변경
+    board = np.array(board)
+    grid = np.zeros((len(board)+1, len(board[0])+1))
+
+    for info in skill:
+        degree = make_degree(info, grid)
+        
+        if info[0] == 1:    # type에 따라 board 업데이트
+            board = board - degree
+        else:
+            board = board + degree
+    
+    # board 내 양수값 개수 카운팅
+    cnt = 0
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] > 0:
+                cnt += 1
+    
+    return cnt
+
+#_________________________________________________________________________________________________________________
+def solution(board, skill):
+    answer = 0
+
+    # 누적합을 위한 2차원 배열 생성
+    arr = [[0]*(len(board[0])+1) for _ in range(len(board)+1)]
+
+    # 각 skill별 누적합 초기값 설정
+    for type, r1, c1, r2, c2, degree in skill:
+        arr[r1][c1] += degree if type == 2 else -degree
+        arr[r1][c2+1] += -degree if type == 2 else degree
+        arr[r2+1][c1] += -degree if type == 2 else degree
+        arr[r2+1][c2+1] += degree if type == 2 else -degree
+    
+    # skill별 누적합이 설정된 2차원 배열을 한꺼번에 누적합 계산 (이게 핵심)
+    import numpy as np
+    arr = np.array(arr)
+    arr = arr.cumsum(axis=0)
+    arr = arr.cumsum(axis=1)
+    arr = np.delete(arr, -1, axis=0)
+    arr = np.delete(arr, -1, axis=1)
+
+    # board값에 누적합 업데이트
+    board = np.array(board) + arr
+
+    # board 내 양수값 개수 카운팅
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] > 0:
+                answer += 1
+    
+    return answer
+
+
+board, skill = [[5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5]], [[1, 0, 0, 3, 4, 4], [1, 2, 0, 2, 3, 2], [2, 1, 0, 3, 1, 2], [1, 0, 1, 3, 3, 1]]
+print(solution(board, skill))
